e6cb25ba6b223fc8107c744f0f7129d60e95c289
ramu siripalli <ramsiripalli2772@gmail.com>
2025-12-12 13:14:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function isSameType(value1, value2) {
   //your js code here
+	let type1 = typeof value1;
+	let type2 = typeof value2;
+	if(type1 == type2) return true;
+	else{
+		return false;
+	}
 }
 
 // do not change the code below.
