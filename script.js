e88f8d0a4e6d8bf03ba21aea4e1b921e22dd570b
ramu siripalli <ramsiripalli2772@gmail.com>
2025-12-19 12:00:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function isSameType(value1, value2) {
 	 if (Number.isNaN(value1) && Number.isNaN(value2)) {
     return true;
   }
+	 if (Number.isNaN(value1) || Number.isNaN(value2)) {
+    return false;
+  } 
 
   // Compare types normally
   return typeof value1 === typeof value2;
